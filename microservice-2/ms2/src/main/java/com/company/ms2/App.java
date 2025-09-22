f6728de6b299c982b1cceef320f209b27ac06ecf
Rajvardhan-128 <rajvardhankale284@gmail.com>
2025-09-22 11:24:31 +0530

    Added 3 microservices

---
+++ microservice-2/ms2/src/main/java/com/company/ms2/App.java
@@ -0,0 +1,13 @@
+package com.company.ms2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
